1944c74cd01f1f3dbae21750c1a50cd58a5c4b1e
Rajas Katole <rajaskatole@gmail.com>
2026-05-29 12:23:23 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+private:
+    vector<int> stack;
+    vector<int> minStack;
+
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        stack.push_back(val);
+        if(minStack.empty() || val <= minStack.back()) {
+            minStack.push_back(val);
+        }
+    }
+    
+    void pop() {
+        if(stack.back() == minStack.back()) {
+            minStack.pop_back();
+        }
+        stack.pop_back();
+    }
+    
+    int top() {
+        return  stack.back();
+    }
+    
+    int getMin() {
+        return minStack.back();
+    }
+};
